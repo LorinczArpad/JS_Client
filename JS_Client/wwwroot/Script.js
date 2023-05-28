7f1b5644516038c26c2233fb4359347b8c96cab2
lorincz.arpad@stud.uni-obuda.hu <lorincz.arpad@stud.uni-obuda.hu>
2023-05-28 13:44:31 +0200

    Add project files.

---
+++ JS_Client/wwwroot/Script.js
@@ -0,0 +1,31 @@
+﻿let allgames = [];
+
+
+
+fetch('http://localhost:60949/game')
+    .then(x => x.json())
+    .then(y => {
+        allgames = y;
+        console.log(allgames);
+        display();
+    })
+
+
+function display() {
+    allgames.forEach(g => {
+       
+        document.getElementById('allgamesarea').innerHTML += `<tr>
+        <td>${g.name}</td>
+        <td>${g.gameID}</td>
+        <td>${g.pyear}</td>
+        <td>${g.studioId}</td>
+        <td>${g.reqId}</td>
+        </tr>`;
+        console.log(g.name);
+
+
+    })
+
+
+
+}
